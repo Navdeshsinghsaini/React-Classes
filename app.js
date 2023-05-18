6d1b54fce5899b1786259268f5fc174af7c8bd43
Navdeshsinghsaini <naveshsinghsaini@gmail.com>
2023-05-18 09:48:10 +0530

    class 1

---
+++ app.js
@@ -0,0 +1,17 @@
+const heading = React.createElement("div" , 
+{ id :"parent" } , React.createElement( "div" , 
+{ id :"child" } , React.createElement("h1" , 
+{ id :"words" } , "Hello React From React..!")));
+
+// const heading = ;
+
+
+
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+
+
+
+    root.render(heading);
+
